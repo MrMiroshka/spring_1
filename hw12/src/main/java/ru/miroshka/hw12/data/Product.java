d9c3f29ee100e9b5501fef256cf8bd969491b4fe
Miroshnichenko Igor <Miroshnichenko.Igor.Valerivich@yandex.ru>
2023-03-18 15:16:59 +0500

    hw 12 init

---
+++ hw12/src/main/java/ru/miroshka/hw12/data/Product.java
@@ -0,0 +1,26 @@
+package ru.miroshka.hw12.data;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_product")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cost")
+    private Integer cost;
+
+}
